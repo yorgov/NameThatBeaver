b281c06b301f5457604517a8df87fc67fb7b16b9
ivan.yorgov@gmail.com <ivan.yorgov@gmail.com>
2024-11-06 02:59:46 +0200

    release 1.4.0 * Added option for periodical redownload of names file in case the source is a remote file * Added option for Refresh of name pool on detected file while ingame

--- NameThatBeaverSettings.cs
+++ NameThatBeaverSettings.cs
@@ -1,9 +1,21 @@
-﻿namespace NameThatBeaver
+﻿using Newtonsoft.Json;
+
+namespace NameThatBeaver
 {
     public class NameThatBeaverSettings
     {
         public string NamesListLocation { get; set; } = string.Empty;
 
         public bool ReuseNames { get; set; } = false;
+
+        public int RedownloadListAfter { get; set; } = 0;
+
+        public bool RefreshNamePoolOnFileChange { get; set; } = false;
+
+        [JsonIgnore]
+        public bool NamesListIsRemote { get; set; }
+
+        [JsonIgnore]
+        public string BeaversNamesFileLocation { get; set; } = string.Empty;
     }
 }
